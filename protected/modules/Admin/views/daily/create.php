a6c79fd0936604595375b9adc5355532aad8b0bc
fangtoby <fangtoby@live.cn>
2013-03-19 17:29:09 +0800

    Yii note

--- protected/modules/Admin/views/daily/create.php
+++ protected/modules/Admin/views/daily/create.php
@@ -1,1 +1,2 @@
-<?=$this->action->id;?>
+<h2>PHP 读写JS文件</h2>
+<h2>PHP 读写exel||csv文件</h2>
--- protected/modules/Admin/views/daily/view.php
+++ protected/modules/Admin/views/daily/view.php
@@ -1,1 +1,10 @@
-<?=$this->action->id;?>
+<h2>破解mysql密码</h2>
+<li>net stop mysql</li>
+<li>mysqld --skip-grant-tables</li>
+<li>mysql -u root</li>
+<li>use mysql</li>
+<li>update mysql.user set password=password("root") where user="root"</li>
+<li>flush privileges</li>
+<li>net start mysql</li>
+<li>mysql -u root -p</li>
+<li>new password</li>
