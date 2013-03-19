f4537fe8a3ef04e050d4d6686b002cced1c43ca0
fangtoby <fangtoby@live.cn>
2013-03-19 16:23:31 +0800

    Xml operating

--- protected/modules/Admin/views/daily/index.php
+++ protected/modules/Admin/views/daily/index.php
@@ -1,12 +1,31 @@
-<?=$this->action->id;?>
-<pre>
+<h2>PHP 读写XML文件</h2>
 <?php
 	//echo Yii::getPathOfAlias('webroot').'/protected/data/songs.xml';
-	$mysongs = simplexml_load_file(Yii::getPathOfAlias('webroot').'/protected/data/songs.xml');
+	$xmlPath = Yii::getPathOfAlias('webroot').'/protected/data/songs.xml';
+	/*
+	$mysongs = simplexml_load_file($xmlPath);
 	foreach($mysongs as $key=>$values)
 	{	
 		echo $values->title."<br />";
 		echo $values->artist."<br />"."<br />"."<br />";
 	}
+	*/
+	$doc = new DOMDocument();
+	$doc->load($xmlPath);
+	$node_liest = $doc->getElementsByTagName('song');
+	for($i=0;$i<$node_liest->length;$i++){
+		$node = $node_liest->item($i);
+		echo $node->getAttribute('dateplayed')."<br />";
+		echo '<b>title:</b>'.$node->getElementsByTagName('title')->item(0)->nodeValue."<br />";
+		echo '<b>artist:</b>'.$node->getElementsByTagName('artist')->item(0)->nodeValue."<br />";
+		if(!($node->getElementsByTagName('detail')->length)){
+			$detail = $doc->createElement('detail');
+			$detail->nodeValue = date('Y-m-d h:i:s',time());
+			$node->appendChild($detail);
+		}else{
+			echo '<b>detail:</b>'.$node->getElementsByTagName('detail')->item(0)->nodeValue."<br /><br /><br />";
+		}
+	}
+	$doc->save($xmlPath);
+	
 ?>
-</pre>
